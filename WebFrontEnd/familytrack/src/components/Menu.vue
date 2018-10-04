9c4705715ae0e1c7f9f628ce1acb5ec0e8873b10
Pablo Marquez <pjmarquez@gmail.com>
2018-10-04 01:15:27 -0300

    comment loggedIn

--- WebFrontEnd/familytrack/src/components/Menu.vue
+++ WebFrontEnd/familytrack/src/components/Menu.vue
@@ -72,15 +72,15 @@ export default {
     ]
   }),
   computed: {
-    loggedIn () {
-      if (this.$store.state.IsAuthenticated) {
-        user.getById(this.$store.state.token, this.$store.state.user.id).then(response => {
-          console.log(response)
-          this.user = response.username
-        })
-      }
-      return this.$store.state.IsAuthenticated
-    }
+    // loggedIn () {
+    //   if (this.$store.state.IsAuthenticated) {
+    //     user.getById(this.$store.state.token, this.$store.state.user.id).then(response => {
+    //       console.log(response)
+    //       this.user = response.username
+    //     })
+    //   }
+    //   return this.$store.state.IsAuthenticated
+    // }
   },
   methods: {
     logout: function (event) {
