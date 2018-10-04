fbf3f51b4a556460b5faa8fc3ec83bb892153e6d
Pablo Marquez <pjmarquez@gmail.com>
2018-10-04 01:29:19 -0300

    MODEL

--- WebBackEnd/familytrack/server/create-lb-tables.js
+++ WebBackEnd/familytrack/server/create-lb-tables.js
@@ -1,10 +1,11 @@
 var server = require('./server');
 var ds = server.dataSources.mysqlds;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Group', 'Group_member', 
-    'Position_member'];
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Group', 
+  'Group_member', 'Position_member'];
 
 ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
-  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  console.log('Loopback tables [' + lbTables + '] created in ', 
+    ds.adapter.name);
   ds.disconnect();
 });
