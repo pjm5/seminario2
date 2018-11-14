4c0efefe39fb9765a0557b8ce2adb5e9290221be
Pablo Marquez <pjmarquez@gmail.com>
2018-11-14 20:34:06 -0300

    fixed test3

--- WebFrontEnd/familytrack/src/components/Menu.vue
+++ WebFrontEnd/familytrack/src/components/Menu.vue
@@ -91,12 +91,6 @@ export default {
   },
   computed: {
     loggedIn () {
-      if (this.$store.state.IsAuthenticated) {
-        user.getById(this.$store.state.token, this.$store.state.user.id).then(response => {
-          console.log(response)
-          this.user = response.username
-        })
-      }
       return this.$store.state.IsAuthenticated
     }
   },
