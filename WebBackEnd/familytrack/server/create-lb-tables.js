cb1b2e9f05a148e2fa1c74b5ee08f3aa926b9d38
Pablo Marquez <pjmarquez@gmail.com>
2019-02-27 21:34:43 +0100

    create user-rol, update db, update relation

--- WebBackEnd/familytrack/server/create-lb-tables.js
+++ WebBackEnd/familytrack/server/create-lb-tables.js
@@ -1,11 +1,12 @@
+
 var server = require('./server');
-var ds = server.dataSources.mysqlds;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Group', 
-  'Group_member', 'Position_member'];
+var ds = server.dataSources.herokudb;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Group',
+  'Group_member', 'Position_member', 'userRol'];
 
 ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
-  console.log('Loopback tables [' + lbTables + '] created in ', 
+  console.log('Loopback tables [' + lbTables + '] created in ',
     ds.adapter.name);
   ds.disconnect();
 });
