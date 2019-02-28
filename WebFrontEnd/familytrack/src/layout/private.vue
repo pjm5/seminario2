d633ff3ee4fb90c2108c9cf78a5f7bb670e51349
Pablo Marquez <pjmarquez@gmail.com>
2019-02-27 21:56:34 -0300

    udpta login, valida user, create user mobile

---
+++ WebFrontEnd/familytrack/src/layout/private.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Menu></Menu>
+    <v-content>
+      <v-container fluid class="grey lighten-4">
+        <router-view/>
+      </v-container>
+    </v-content>
+  </div>
+</template>
+
+<script>
+import Menu from "@/components/Menu";
+
+export default {
+  name: 'Private',
+  data: () => ({}),
+  props: {
+    source: String
+  },
+  mounted() {},
+  created() {},
+  components: {
+    Menu
+  }
+};
+</script>
---
+++ WebFrontEnd/familytrack/src/layout/public.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'Public',
+  data: () => ({}),
+  props: {
+    source: String
+  },
+  mounted() {},
+  created() {}
+};
+</script>
